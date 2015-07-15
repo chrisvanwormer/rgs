81c1a9a0d50dbee55f9143764e51af24e111bc86
Chris VanWormer <chris.vanwormer@clickitdigital.com>
2015-07-15 12:19:28 -0400

    changes prior to launch week

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
+
+$(document).ready(function() {
+    $('#sendContact').on('click', function() {
+        // grab form field data
+        var formFirstName    = $('#firstName').val();
+        var formLastName    = $('#lastName').val();
+        var formEmail   = $('#emailAddress').val();
+        var formMessage = $('#message').val();
+        var formdata    = 'firstname=' + formFirstName + '&lastname=' + formLastName + '&email=' + formEmail + '&message=' + formMessage;
+        $('#content-thank-you').foundation('reveal', 'open', {
+            url: '/foundation/php/app.php?script=message',
+            type: 'POST',
+            data: formdata,
+            success: function(data) {
+                //$('#messageName').val('');
+                //$('#messageEmail').val('');
+                //$('#messageMessage').val('');
+            },
+            error: function(data) {
+
+                var html = '<img src="img/logo_black.svg" /><h4 class="clickit-section-centered-text">We\'re sorry. Something funky happened.  Please try again.</h4><a class="close-reveal-modal">&#215;</a>';
+                $('#content-thank-you').html(html);
+                $('#content-thank-you').foundation('reveal', 'open');
+            }
+        });
+        return false;
+    });
+});
+
 $(document).foundation();
